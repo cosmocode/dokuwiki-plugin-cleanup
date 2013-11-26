0edb65b7b8011c14671530d817fa04f453e222e0
Andreas Gohr <gohr@cosmocode.de>
2013-11-26 14:29:31 +0100

    added German translation

---
+++ lang/de/lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * German language file for maintenance plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+$lang['menu'] = 'Wartung: Datei-Bereinigung';
+
+// custom language strings for the plugin
+$lang['sum']     = '%d Dateien mit einem Platzbedarf von %s wurden gefunden.';
+$lang['preview'] = 'Betroffene Dateien auflisten';
+$lang['execute'] = 'Betroffene Dateien löschen';
+$lang['done']    = 'Die Dateien wurden gelöscht.';
+
+
+//Setup VIM: ex: et ts=4 :
---
+++ lang/de/settings.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * german language file for maintenance plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ */
+
+$lang['runautomatically'] = 'Den Datei-Bereinigungsvorgang automatisch ausführen?';
+
+$lang['cacheage'] = 'Lösche Cache-Dateien nach dieser Zeit (Sekunden).';
+$lang['atticage'] = 'Lösche alte Seiten-Versionen nach dieser Zeit (Sekunden).';
+$lang['atticnoexonly'] = 'Lösche nur alte Seiten-Versionen zu denen keine aktuelle Version mehr existiert (nach obiger Zeit).';
+$lang['mediaatticage'] = 'Lösche alte Medien-Versionen nach dieser Zeit (Sekunden).';
+$lang['mediaatticnoexonly'] = 'Lösche nur alte Medien-Versionen zu denen keine aktuelle Version mehr existiert (nach obiger Zeit).';
+$lang['metaage'] = 'Lösche die Meta-Daten zu nicht existierenden Seiten nach dieser Zeit (Sekunden).';
+$lang['mediametaage'] = 'Lösche die Meta-Daten zu nicht existierenden Medien-Dateien nach dieser Zeit (Sekunden).';
+$lang['lockage'] = 'Lösche alte Lock-Files nach dieser Zeit (Sekunden).';
+
+//Setup VIM: ex: et ts=4 :
