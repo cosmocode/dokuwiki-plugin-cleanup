09a159a89cf41ae46570d1b0df2c267821f1b95a
Andreas Gohr <gohr@cosmocode.de>
2013-11-26 14:20:08 +0100

    another typo fix

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -9,7 +9,7 @@
 $lang['menu'] = 'Maintenance Clean-Up';
 
 // custom language strings for the plugin
-$lang['sum'] = '%d files using %s of space where found.';
+$lang['sum'] = '%d files using %s of space were found.';
 $lang['preview'] = 'Preview affected files';
 $lang['execute'] = 'Delete affected files';
 $lang['done'] = 'The files have been deleted.';
