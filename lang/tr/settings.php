24d5d54efb56e0c625c80445289bc8374cc443cf
İlker Rifat Kapaç <irifat@gmail.com>
2015-05-27 01:18:22 +0300

    Create settings.php
    settings file for Turkish translation

---
+++ lang/tr/settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Turkish language file for cleanup plugin
+ *
+ * @author Andreas Gohr <gohr@cosmocode.de>
+ * translate İlker R. Kapaç <irifat@gmail.com>
+ */
+
+$lang['runautomatically'] = 'Dosya temizleme işlemi otomatik olarak çalıştırılsın mı?';
+
+$lang['cacheage'] = 'Ara bellek dosyalarını bu kadar süre sonra sil (saniye).';
+$lang['atticage'] = 'Eski sayfalara ait düzeltmeleri bu kadar süre sonra sil (saniye).';
+$lang['atticnoexonly'] = 'Yukarıdaki kadar süre sonra güncel düzeltme kalmadıysa, sadece eski sayfaların düzeltmelerini sil.';
+$lang['mediaatticage'] = 'Görsellerin eski düzeltmelerini bu kadar süre sonra sil (saniye).';
+$lang['mediaatticnoexonly'] = 'Yukarıdaki kadar süre sonra güncel düzeltme kalmadıysa, sadece eski görsellerin düzeltmelerini sil.';
+$lang['metaage'] = 'Var olmayan sayfaların üstverilerini (meta bilgilerini) bu kadar süre sonra sil (saniye).';
+$lang['mediametaage'] = 'Var olmayan görsellerin üstverilerini (meta bilgilerini) bu kadar süre sonra sil (saniye).';
+$lang['lockage'] = 'Durum kilidi dosyalarını bu kadar süre sonra sil (saniye).';
+
+
+
+//Setup VIM: ex: et ts=4 :
