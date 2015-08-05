822df1cc2c47ddaeed89890c1ccba5eb345d1de1
trebmuh <trebmuh@users.noreply.github.com>
2015-08-05 23:21:21 +0200

    Create  settings.php
    File created for the initial French translation

---
+++ lang/fr/ settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * french language file for cleanup plugin
+ *
+ * @author Olivier Humbert <trebmuh@tuxfamily.org>
+ */
+
+$lang['runautomatically'] = 'Lancer le traitement de nettoyage de fichier automatiquement ?';
+
+$lang['cacheage'] = 'Effacer les fichiers en cache après cette durée (en secondes).';
+$lang['atticage'] = 'Effacer les anciennes révisions de pages après cette durée (en secondes).';
+$lang['atticnoexonly'] = 'Efface les révisions des anciennes pages seulement quand il n\'y a plus de révision courante (après la durée ci-dessus).';
+$lang['mediaatticage'] = 'Effacer les anciennes révisions de média après cette durée (en secondes).';
+$lang['mediaatticnoexonly'] = 'Efface les révisions des anciens médias seulement quand il n\'y a plus de révision courante (après la durée ci-dessus).';
+$lang['metaage'] = 'Effacer les métadonnées des pages inexistantes après cette durée (en secondes).';
+$lang['mediametaage'] = 'Effacer les métadonnées des médias inexistants après cette durée (en secondes).';
+$lang['lockage'] = 'Effacer les fichiers stale lock après cette durée (en secondes).';
+
+
+
+//Setup VIM: ex: et ts=4 :
