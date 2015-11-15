0645e93d3b683cb2ae95d5865f88bd06fde1400d
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2015-11-15 13:10:06 +0100

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ */
+$lang['menu']                  = ' Čištění souborů';
+$lang['sum']                   = 'Bylo nalezeno %d souborů využívajících %s místa.';
+$lang['preview']               = 'Náhled vybraných souborů';
+$lang['execute']               = 'Smazat vybrané soubory';
+$lang['done']                  = 'Soubory byly smazány.';
---
+++ lang/cs/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ */
+$lang['runautomatically']      = 'Spouštět čistící proces automaticky?';
+$lang['cacheage']              = 'Mazat odkládací soubory po tomto časovém intervalu (sekundy).';
+$lang['atticage']              = 'Mazat staré revize stránke po tomto časovém intervalu (sekundy).';
+$lang['atticnoexonly']         = 'Mazat pouze staré revize stránek, pro které neexistuje žádná aktuální revize (po čase výše).';
+$lang['mediaatticage']         = 'Mazat staré revize médií po tomto časovém intervalu (sekundy).';
+$lang['mediaatticnoexonly']    = 'Mazat pouze staré revize médií, pro které neexistuje žádná aktuální revize (po čase výše).';
+$lang['metaage']               = 'Mazat metadata neexistujících stránek po tomto časovém intervalu (sekundy).';
+$lang['mediametaage']          = 'Mazat metadata neexistujících stránek po tomto časovém intervalu (sekundy).';
+$lang['lockage']               = 'Mazat vypršené uzamykací soubory po tomto časovém intervalu (sekundy).';
