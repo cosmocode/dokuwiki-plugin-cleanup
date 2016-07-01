7bc30e66914d61bf2e15ea30cdcddf3f92e7156e
Myeongjin <aranet100@gmail.com>
2016-07-01 11:45:06 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['menu']                  = '파일 정리';
+$lang['sum']                   = '공간의 %s을(를) 사용하는 파일 %d개를 찾았습니다.';
+$lang['preview']               = '영향 받는 파일 미리 보기';
+$lang['execute']               = '영향 받는 파일 삭제';
+$lang['done']                  = '파일이 삭제되었습니다.';
