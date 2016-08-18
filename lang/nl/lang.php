b033a20000bd7ae4b69dc55fdcfebb558ce0b517
Marcel Bachus <marcel.bachus@ziggo.nl>
2016-08-18 12:15:07 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marcel Bachus <marcel.bachus@ziggo.nl>
+ */
+$lang['menu']                  = 'Bestanden Opschonen';
+$lang['sum']                   = '%d bestanden gebruiken %s ruimte zijn gevonden.';
+$lang['preview']               = 'Voorbeeld tonen van geselecteerde bestanden.';
+$lang['execute']               = 'Verwijder geselecteerde bestanden';
+$lang['done']                  = 'De bestanden zijn verwijderd.';
---
+++ lang/nl/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Marcel Bachus <marcel.bachus@ziggo.nl>
+ */
+$lang['runautomatically']      = 'Start het opschoon proces automatisch?';
+$lang['cacheage']              = 'Verwijder cache bestanden na deze tijd (seconden).';
+$lang['atticage']              = 'Verwijder oude pagina revisies na deze tijd (seconden).';
