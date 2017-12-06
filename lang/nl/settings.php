05d9979a76779bdb6d662178ca8c4494d3c8249d
Coen Eisma <info@coeneisma.nl>
2017-12-06 15:00:32 +0100

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -3,8 +3,15 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Coen Eisma <info@coeneisma.nl>
  * @author Marcel Bachus <marcel.bachus@ziggo.nl>
  */
 $lang['runautomatically']      = 'Start het opschoon proces automatisch?';
 $lang['cacheage']              = 'Verwijder cache bestanden na deze tijd (seconden).';
 $lang['atticage']              = 'Verwijder oude pagina revisies na deze tijd (seconden).';
+$lang['atticnoexonly']         = 'Verwijder alleen oude paginarevisies als er geen huidige revisieversie meer aanwezig is (na bovenstaand tijdstip).';
+$lang['mediaatticage']         = 'Verwijder oude mediarevisies na deze tijd (in seconden).';
+$lang['mediaatticnoexonly']    = 'Verwijder alleen oude mediarevisies als er geen huidige revisieversie meer aanwezig is (na bovenstaand tijdstip).';
+$lang['metaage']               = 'Verwijder metadata van niet bestaande pagina\'s na deze tijd (in seconden).';
+$lang['mediametaage']          = 'Verwijder metadata van niet bestaande media na deze tijd (in seconden).';
+$lang['lockage']               = 'Verwijder oude blokkeringsbestanden na deze tijd (in seconden).';
