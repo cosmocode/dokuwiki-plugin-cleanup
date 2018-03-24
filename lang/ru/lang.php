58c50b4f2e60ff72e6ede76d52baf2bd4d010be3
Yuriy Skalko <yuriy.skalko@gmail.com>
2018-03-25 00:55:36 +0100

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Yuriy Skalko <yuriy.skalko@gmail.com>
+ */
+$lang['menu']                  = 'Очистка файлов';
+$lang['sum']                   = 'Найдено %d файлов, использующих объём %s.';
+$lang['preview']               = 'Просмотреть соответствующие файлы';
+$lang['execute']               = 'Удалить соответствующие файлы';
+$lang['done']                  = 'Файлы были удалены.';
