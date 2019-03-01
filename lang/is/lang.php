f5ce24bff0783738ed4796ae5915ffb77f1d8556
Magnús Ýmir Magnússon <mm@mck-dk.dk>
2019-03-01 13:10:11 +0100

    translation update

---
+++ lang/is/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Magnús Ýmir Magnússon <mm@mck-dk.dk>
+ */
+$lang['done']                  = 'Skránum hefur verið eytt.';
