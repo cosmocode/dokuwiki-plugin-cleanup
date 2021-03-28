32afa00c6eac9bc6d92b0a05abef0089616614ca
吾为天歌 <3035134331@qq.com>
2021-03-28 17:30:05 +0200

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 吾为天歌 <3035134331@qq.com>
+ */
+$lang['menu']                  = '文件清理';
+$lang['preview']               = '预览受影响的文件';
+$lang['execute']               = '删除受影响的文件';
+$lang['done']                  = '文件已被删除。';
